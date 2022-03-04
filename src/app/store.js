19db952c18c57b74c47db8dff8a9cc8522561d46
Szymon <szymon@MacBook-Pro-Mac-2.local>
2022-03-04 13:48:40 +0000

    Succesfull fetching data for chart1 update

--- src/app/store.js
+++ src/app/store.js
@@ -15,11 +15,11 @@ const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [sagaMiddleware];
 
-const store2 = configureStore({
+const store = configureStore({
     reducer,
     middleware: [...middleware]
 })
 
 sagaMiddleware.run(watcherSaga);
 
-export default store2
+export default store
