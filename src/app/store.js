b13c5f8dfd00769658adc724116c5f0fe7498003
Szymon <szymon@MacBook-Pro-Mac-2.local>
2022-03-04 23:36:33 +0000

    Redux-persist implemented for user state

--- src/app/store.js
+++ src/app/store.js
@@ -2,14 +2,23 @@ import { combineReducers } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { watcherSaga } from "../sagas/rootSaga";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
 import chart1Reducer from "../ducks/chart1";
 import chart2Reducer from "../ducks/chart2";
 import chart3Reducer from "../ducks/chart3";
 import userReducer from "../ducks/user"
 
+const persistConfig = {
+    key: 'persist-key',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
 const reducer = combineReducers({
-    user: userReducer,
+    user: persistedReducer,
     chart1: chart1Reducer,
     chart2: chart2Reducer,
     chart3: chart3Reducer,
@@ -24,6 +33,9 @@ const store = configureStore({
     middleware: [...middleware]
 })
 
+const persistor = persistStore(store)
+
 sagaMiddleware.run(watcherSaga);
 
 export default store
+export {persistor}
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import store from './app/store'
+import store, {persistor} from './app/store'
+
+import {PersistGate} from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
